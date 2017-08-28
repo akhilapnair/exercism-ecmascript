9e60c9363f970102edb87c9886e600d9e7f4b23c
akhilapnair <akhi.prabha@gmail.com>
2017-08-28 09:35:37 +0530

    Initial Commit

---
+++ pascals-triangle/pascals-triangle.js
@@ -0,0 +1,29 @@
+class Triangle {
+
+constructor(row) {
+	let rows = [1];
+	// for(let n=0;n<row;n++){
+	// 	rows.push("1");
+	// }
+// 	  let n_row = [];
+	
+// 	for(let k=0;k<n;k++) {
+	  
+//    		rows = n_row.push(choice(n,k));
+// }
+// }
+// const choice = (n,k)=> {
+// 	if( n==0 || k==0) {
+// 	  	let ncr = 1;
+// 	  	n_row.push(ncr);
+// 	  }
+// 	  else {
+// 	  	ncr = ncr*(n-k+1)/k;
+// 	  	n_row.push(ncr);
+// 	  }
+// 	  return n_row;
+// };
+}
+}
+
+export default Triangle;
