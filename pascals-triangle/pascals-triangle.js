bcb31fda36c433eec27d898557836a40c354e5a5
akhilapnair <akhi.prabha@gmail.com>
2017-08-30 10:05:46 +0530

    pascals three to last row

--- pascals-triangle/pascals-triangle.js
+++ pascals-triangle/pascals-triangle.js
@@ -9,9 +9,19 @@ constructor(n_row) {
 			rows.push(row);
 	}
 	this.rows = rows;
+
 }
+ get lastRow() {
+    return this.rows[this.rows.length - 1];
+  }
 	 
 }
 const n_choice_k = (n,k)=> {
 	if( n-k<=0 || k<=0) return 1
-};
+		return fact(n)/fact(k)/fact(n-k);
+},
+fact =(n) => {
+	let result = 1;
+	for (let i=2;i<=n ;i++) result *= i;
+		return result;
+}
