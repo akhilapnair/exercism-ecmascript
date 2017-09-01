68060d7d34687d472bd07600c0042561fea3269b
akhilapnair <akhi.prabha@gmail.com>
2017-09-01 12:43:42 +0530

    pascals triangle completion

--- pascals-triangle/pascals-triangle.js
+++ pascals-triangle/pascals-triangle.js
@@ -1,7 +1,7 @@
 export default class Triangle {
 
 constructor(n_row) {
-    let rows = [];
+	let rows = []; 
 	for(let n=0;n<n_row;n++){
 		let row = [];
 		for(let k=0;k<=n;k++)
@@ -9,14 +9,12 @@ constructor(n_row) {
 			rows.push(row);
 	}
 	this.rows = rows;
-
 }
  get lastRow() {
-    return this.rows[this.rows.length - 1];
+	    return this.rows[this.rows.length - 1];
   }
-	 
 }
-const n_choice_k = (n,k)=> {
+  const n_choice_k = (n,k)=> {
 	if( n-k<=0 || k<=0) return 1
 		return fact(n)/fact(k)/fact(n-k);
 },
@@ -25,3 +23,6 @@ fact =(n) => {
 	for (let i=2;i<=n ;i++) result *= i;
 		return result;
 }
+	 
+
+
